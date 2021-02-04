d3330001995c885b83a19ad47f7f70359272f54a
Maria Helena Santos Pineda <msantospineda1@gmail.com>
2021-02-04 12:22:26 -0500

    Probando Mongoose - CRUD -

---
+++ models/Alumno.js
@@ -0,0 +1,19 @@
+
+const { Schema, model } = require('mongoose');
+
+const AlumnoSchema = new Schema({
+    nombres:{
+        type: String, required:true,
+    },
+    apellidos:{
+        type: String, required:true,
+    },
+    edad:{
+        type: Number, required:true,
+    },
+    created_since: {
+        type:Date, default:Date.now,
+    }
+});
+
+module.exports = model('Alumno',AlumnoSchema);
